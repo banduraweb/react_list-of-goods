49d813e4d9c510a4e1c7f11c04ef4231f7ecaae6
bandura <banduraweb@ukr.net>
2019-12-09 04:49:01 +0200

    Updates

---
+++ precache-manifest.a9cbd7ade52bdd1c54b9f9d5d051613d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fc5e726b5e1bcf3736c6d3ee614a1a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "0fbfeabb8d36ff09fdd3",
+    "url": "/react_list-of-goods/static/css/main.bcbc3c9a.chunk.css"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/react_list-of-goods/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "0fbfeabb8d36ff09fdd3",
+    "url": "/react_list-of-goods/static/js/main.1efe5015.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.a9cbd7ade52bdd1c54b9f9d5d051613d.js"
 );
 
 self.addEventListener('message', (event) => {
